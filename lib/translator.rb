42f22f6e87242f297ee38a8c4a1e552674ac49de
Vincent Ivos <vincentivos1@gmail.com>
2020-09-26 22:33:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
+require "yaml"
 # require modules here
 
 def load_library
+  emoticons = YAML.load_file(path)
+  emoticon_hash = ["take_emoticon"] = {}
+  emoticon_hash = {"take_meaning"] = {}
+  
+  
+  
+  
   # code goes here
 end
 
 def get_japanese_emoticon
+    emoticons = YAML.load_file(path)
   # code goes here
 end
 
 def get_english_meaning
+    emoticons = YAML.load_file(path)
   # code goes here
 end

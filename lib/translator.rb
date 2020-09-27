a034cbde3a6f68aeb51c56d46c83c214a0d606b9
Vincent Ivos <vincentivos1@gmail.com>
2020-09-27 01:03:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ end
 def get_japanese_emoticon (emoticon_hash,emoticon)
     emoticon_hash = load_library("./lib/emoticons.yml")
     emoticon_hash.each do |key,value|
-    if value[:japanese] == emoticon 
+    if value[:english] == emoticon 
        return key
     end
   end
@@ -34,7 +34,7 @@ def get_english_meaning(emoticon_hash,emoticon)
   # code goes here
     emoticon_hash = load_library("./lib/emoticons.yml")
     emoticon_hash.each do |key,value|
-    if value[:english] == emoticon 
+    if value[:japanese] == emoticon 
        return key
     end
   end
